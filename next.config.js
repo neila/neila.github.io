7e29c82cd8b4dc9e2cb6c6be8affc6f9071e049f
SHØ <neila@users.noreply.github.com>
2023-04-14 10:44:22 +0900

    update logos

--- next.config.js
+++ next.config.js
@@ -2,17 +2,15 @@ module.exports = {
   reactStrictMode: true,
   images: {
     domains: [
+      "avatars.githubusercontent.com",
+      "cdn-icons-png.flaticon.com",
+      "firebase.google.com",
+      "gist.githubusercontent.com",
+      "raw.githubusercontent.com",
+      "starbeamrainbowlabs.com",
       "upload.wikimedia.org",
       "www.r-project.org",
-      "starbeamrainbowlabs.com",
-      "assets.vercel.com",
-      "firebase.google.com",
       "www.rust-lang.org",
-      "solana.com",
-      "cdn-icons-png.flaticon.com",
-      "raw.githubusercontent.com",
-      "gist.githubusercontent.com",
-      "s3.eu-west-2.amazonaws.com",
     ],
   },
   serverRuntimeConfig: {
