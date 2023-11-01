4ad42b55db2eca379178711e53ec8739b598dd36
neila <neila@shiftbase.xyz>
2023-11-01 14:25:46 +0900

    optimize responsive layout

--- src/pages/art/index.tsx
+++ src/pages/art/index.tsx
@@ -15,42 +15,31 @@ import p35 from "public/images/art/polygon35.png";
 import dghn from "public/images/art/reika1.svg";
 import swlp from "public/images/art/sweetlips.png";
 
-const Title = (params: { section: string; color: string }) => (
-  <div className="mt-5 mb-7 flex-grow-0 text-center">
-    <h1 className={params.color}> {params.section} </h1>
-  </div>
-);
-
-const ArtWork = (props: {
-  colStart: string;
-  colSpan: string;
-  image: StaticImageData;
-  caption: string;
-}) => {
-  const { colStart, colSpan, image, caption } = props;
-  return (
-    <div
-      className={`flex flex-col col-start-${colStart} col-span-${colSpan} m-8`}
-    >
-      <Image
-        className="h-full w-full cursor-pointer object-contain"
-        src={image}
-        alt={caption}
-        style={{
-          maxWidth: "100%",
-          height: "auto",
-        }}
-      />
-      <div className="w-full">
-        <div className="mx-auto w-4/5 text-center">
-          <p className="text-sm font-hairline desktop:font-thin">{caption}</p>
-        </div>
-      </div>
+const Art: NextPage = () => {
+  const Title = (params: { section: string; color: string }) => (
+    <div className="mt-5 mb-7 flex-grow-0 text-center">
+      <h1 className={params.color}> {params.section} </h1>
     </div>
   );
-};
 
-const art: NextPage = () => {
+  const ArtWork = (props: {
+    image: StaticImageData;
+    caption: string;
+  }) => {
+    const { image, caption } = props;
+    return (
+      <div className="text-center">
+        <Image
+          className="cursor-pointer object-contain"
+          src={image}
+          alt={caption}
+        />
+
+        <p className="text-md font-thin">{caption}</p>
+      </div>
+    );
+  };
+
   return (
     <div className="art">
       <Head>
@@ -64,52 +53,17 @@ const art: NextPage = () => {
       <Title section="GALLERY" color="text-[#F726DB]" />
 
       {/* content */}
-      <section className="">
-        <div className="mx-auto flex flex-wrap justify-center p-4 desktop:grid desktop:w-4/5 desktop:grid-cols-6">
-          <ArtWork
-            colStart="1"
-            colSpan="2"
-            image={bbl}
-            caption="Your Love Can Start A Universe"
-          />
-          <ArtWork
-            colStart="3"
-            colSpan="2"
-            image={btt}
-            caption="Eaten By Growth"
-          />
-          <ArtWork colStart="5" colSpan="2" image={dghn} caption="零華" />
-          <ArtWork
-            colStart="1"
-            colSpan="2"
-            image={swlp}
-            caption="I Wanna Eat You"
-          />
-          <ArtWork
-            colStart="3"
-            colSpan="2"
-            image={cdflp}
-            caption="Candy Effect"
-          />
-          <ArtWork
-            colStart="5"
-            colSpan="2"
-            image={rwkng}
-            caption="RUNAWAY KING"
-          />
-          <ArtWork
-            colStart="1"
-            colSpan="2"
-            image={drd}
-            caption="Thoughts I shouldn't have"
-          />
-          <ArtWork colStart="3" colSpan="2" image={p35} caption="Polygon 3.5" />
-          <ArtWork
-            colStart="5"
-            colSpan="2"
-            image={gsh}
-            caption="GALACTIC SUPERHIGHWAY"
-          />
+      <section>
+        <div className="grid grid-cols-[repeat(auto-fit,minmax(320px,1fr))] gap-12">
+          <ArtWork image={bbl} caption="Your Love Can Start A Universe" />
+          <ArtWork image={btt} caption="Eaten By Growth" />
+          <ArtWork image={dghn} caption="零華" />
+          <ArtWork image={swlp} caption="I Wanna Eat You" />
+          <ArtWork image={cdflp} caption="Candy Effect" />
+          <ArtWork image={rwkng} caption="RUNAWAY KING" />
+          <ArtWork image={drd} caption="Thoughts I shouldn't have" />
+          <ArtWork image={p35} caption="Polygon 3.5" />
+          <ArtWork image={gsh} caption="GALACTIC SUPERHIGHWAY" />
         </div>
       </section>
 
@@ -119,4 +73,4 @@ const art: NextPage = () => {
   );
 };
 
-export default art;
+export default Art;
