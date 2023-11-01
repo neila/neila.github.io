68cfe31e164dc981279246a661c66674f3b58c50
neila <neila@shiftbase.xyz>
2023-11-01 14:25:18 +0900

    update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -46,45 +46,11 @@ module.exports = {
       decimal: "decimal",
       square: "square",
     },
-    extend: {
-      typography: {
-        DEFAULT: {
-          css: {
-            color: "#F9EFF1",
-            a: {
-              color: "#F9EFF1",
-              "&:hover": {
-                color: "#F8D106",
-              },
-            },
-            h1: {
-              color: "#F9EFF1",
-              fontWeight: "800",
-            },
-            h2: {
-              color: "#F9EFF1",
-              fontWeight: "700",
-            },
-            h3: {
-              color: "#F9EFF1",
-              fontWeight: "600",
-            },
-            h4: {
-              color: "#F9EFF1",
-              fontWeight: "500",
-            },
-          },
-        },
-      },
-    },
   },
   variants: {
     extend: {
       animation: ["group-hover"],
     },
   },
-  plugins: [
-    require("@tailwindcss/aspect-ratio"),
-    require("@tailwindcss/typography"),
-  ],
+  plugins: [require("@tailwindcss/aspect-ratio")],
 };
