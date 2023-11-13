a2e17480fde590c1eef4f1d39a7ad82d1999c9e6
neila <neila@shiftbase.xyz>
2023-11-13 21:11:21 +0900

    fix state management

--- src/components/YourLife.tsx
+++ src/components/YourLife.tsx
@@ -1,35 +1,47 @@
 import { useEffect, useState } from "react";
 
-const Cells = (props: { date }) => {
-  const submitDate = async (submitDate) => {
-    await fetch("/api/saveBirthday", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(submitDate),
-    });
+const Cells = () => {
+  const getCookie = () => {
+    const value = `; ${document.cookie}`;
+    const parts = value.split("; userDate=");
+    if (parts.length === 2) {
+      return parts.pop().split(";").shift();
+    }
+    return null;
   };
 
-  const [birthDay, setBirthday] = useState(props.date);
+  const [birthDay, setBirthday] = useState("");
 
-  useEffect(() => {
-    submitDate(birthDay);
-  }, [birthDay]);
-
-  const birthDate = +new Date(birthDay);
+  const birthDate = new Date(birthDay);
 
   const now = new Date();
   const year = now.getFullYear();
   const month = now.getMonth();
   const day = now.getDate();
 
-  const today = +new Date(year, month, day);
+  const today = new Date(year, month, day);
 
-  const diffTime = today - birthDate;
+  const diffTime = +today - +birthDate;
 
   const daysSinceBirth = diffTime / (1000 * 60 * 60 * 24);
   const daysOndeath = 365.25 * 85;
+
+  const setCookie = (submitDate: string) => {
+    document.cookie = `userDate=${submitDate}; path=/; expires=${today.setDate(
+      today.getDate() + 400,
+    )}`;
+  };
+
+  useEffect(() => {
+    const cookie = getCookie();
+    if (cookie) {
+      setBirthday(cookie);
+    }
+  }, []);
+
+  useEffect(() => {
+    setCookie(birthDay);
+  }, [birthDay]);
 
   const cellClickModal = (i) => {
     i + 1 < daysSinceBirth / 7
@@ -43,13 +55,14 @@ const Cells = (props: { date }) => {
 
   return (
     <div className="flex flex-col flex-grow justify-center items-center">
+      <p>system birthday: {birthDay}</p>
       <p>
         your birthday:{" "}
         <input
           type="date"
           name="birthday"
           className="text-black"
-          value={birthDay ? birthDay : props.date}
+          value={birthDay ? birthDay : ""}
           onChange={(e) => {
             setBirthday(e.target.value);
           }}
@@ -57,7 +70,7 @@ const Cells = (props: { date }) => {
       </p>
 
       <div className="p-8 py-4 leading-3">
-        {birthDay !== "" ? (
+        {birthDay ? (
           [...Array(Math.floor(daysOndeath / 7))].map((_, i) => (
             <input
               type="checkbox"
@@ -73,9 +86,9 @@ const Cells = (props: { date }) => {
         )}
 
         <p className="py-4 text-center">
-          {birthDate
+          {birthDay
             ? `生後${Math.ceil(
-                (today - birthDate) / (1000 * 60 * 60 * 24 * 7),
+                (+today - +birthDate) / (1000 * 60 * 60 * 24 * 7),
               )}週目/`
             : ""}
           推定寿命
