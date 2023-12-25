bb8a4dbc59c0df035ba488c146049c987557f637
neila <neila@shiftbase.xyz>
2023-12-25 17:22:21 +0900

    move debug and archive paths

---
+++ src/pages/_archives/ideas/index.tsx
@@ -0,0 +1,163 @@
+import fs from "fs";
+
+import Head from "next/head";
+import Link from "next/link";
+import React from "react";
+
+import path from "path";
+import dateFormat from "dateformat";
+import matter from "gray-matter";
+
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
+import Graph from "@/utils/Graph";
+
+type Post = {
+  id: string;
+  type: string;
+  date_created: string;
+  last_edited: string;
+  tagas: string;
+};
+
+const Title = (params: { section: string; color: string }) => (
+  <div className="mt-5 mb-7 flex-grow-0 text-center">
+    <h1 className={params.color}> {params.section} </h1>
+  </div>
+);
+
+const ideas = ({ postsList }) => {
+  return (
+    <div>
+      <Head>
+        <title>XDEA</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      {/* logo */}
+      <Header />
+
+      <Title section="ἸΔΈΑ" color="text-[#FFDF00]" />
+
+      {/*-- content - */}
+      <div className="min-h-full p-8">
+        <div className="mx-auto max-w-4xl space-y-16">
+          {/* intro */}
+          <section className="items-start desktop:flex">
+            <div className="mx-auto space-y-5 leading-snug desktop:w-4/5">
+              <p>
+                My biological brain is far too{" "}
+                <a
+                  href="https://doi.apa.org/doiLanding?doi=10.1037%2F0278-7393.20.5.1063"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  forgetful
+                </a>
+                . The raw thoughts that continuously pass through my mind can
+                only accumulate for so long. Despite the constant mental
+                wrestling, I feel often left stranded in the ponderer&apos;s
+                void, with no wisdom to bear.
+              </p>
+              <p>
+                I find this quite unfortunate, since all brilliant ideas are raw
+                thoughts at the point of its inception. I created this space to
+                nurture these seeds of imagination, so that they can grow into
+                something bigger. In the conscious and continuous effort to
+                acknolwedge and appreciate the everchanging present, lies the
+                art of spontaneous creativity. I named this page after the{" "}
+                <a
+                  href="https://en.wikipedia.org/wiki/Idea"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  ancient Greek word
+                </a>
+                , to which we still owe its use today.
+              </p>
+              <div className="relative mx-auto desktop:w-3/5">
+                <blockquote className="relative border-l-4 p-4 text-center text-xl italic">
+                  <p className="mb-4">&quot;Ego cogito, ergo sum&quot;</p>
+                  <cite className="ml-16 text-base">- René Descartes</cite>
+                </blockquote>
+              </div>
+            </div>
+          </section>
+
+          <ul className="flex flex-col space-y-2 px-0">
+            {postsList.map(
+              (post: { id: string; date_created: string; tags: string }) => (
+                <div
+                  key={post.id}
+                  className="cursor-pointer rounded-md bg-gradient-to-r from-purple-700 via-blue-400 to-green-500 px-3 align-middle hover:from-pink-500 hover:to-yellow-500"
+                >
+                  <Link
+                    href={{
+                      pathname: `/ideas/${encodeURIComponent(post.id)}`,
+                    }}
+                  >
+                    <div>
+                      <h3 className="truncate text-base desktop:text-lg">
+                        {post.id}
+                      </h3>
+                      <p className="truncate text-xs desktop:text-sm">
+                        {dateFormat(post.date_created, "yyyy-mm-dd", true)} |{" "}
+                        {post.tags}
+                      </p>
+                    </div>
+                  </Link>
+                </div>
+              ),
+            )}
+          </ul>
+        </div>
+      </div>
+
+      <div id="graph-container">
+        <Graph />
+      </div>
+
+      {/* Lower menu + copyright */}
+      <Footer />
+    </div>
+  );
+};
+
+export async function getStaticProps() {
+  const postsDirectory = path.join(process.cwd(), "public/posts");
+  const fileNames = fs.readdirSync(postsDirectory, "utf8");
+
+  const allPostsData = fileNames.map((fileName: string) => {
+    // Remove ".md" from file name to get id
+    const id = fileName.replace(/\.md$/, "");
+
+    // Read markdown file as string
+    const fullPath = path.join(postsDirectory, fileName);
+    const fileContents = fs.readFileSync(fullPath, "utf8");
+
+    // Use gray-matter to parse the post metadata section
+    const postMetadata = matter(fileContents).data;
+
+    // Combine the data with the id
+    return {
+      id,
+      ...postMetadata,
+    };
+  });
+
+  const sorted_allPostsData = allPostsData.sort((a: Post, b: Post) => {
+    if (a.date_created < b.date_created) {
+      return 1;
+    }
+    return -1;
+  });
+  const postsList = JSON.parse(JSON.stringify(sorted_allPostsData));
+
+  return {
+    props: {
+      postsList,
+    },
+  };
+}
+
+export default ideas;
---
+++ src/pages/_archives/works/radio/index.tsx
@@ -0,0 +1,90 @@
+import Head from "next/head";
+import Image from "next/image";
+
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
+
+import Songrec from "public/images/radio/songrec.png";
+
+const Title = (params: { section: string; color: string }) => (
+  <div className="mt-5 mb-7 flex-grow-0 text-center">
+    <h1 className={params.color}> {params.section} </h1>
+  </div>
+);
+
+export default function radio() {
+  return (
+    <div className="">
+      <Head>
+        <title>Radio algorithm - SHØ</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Header />
+
+      <div className="relative min-h-full px-8 desktop:mx-24">
+        <div className="relative flex justify-between px-2">
+          <div className="relative h-full flex-grow text-[#C9CBC5]">
+            <Title section="Radio algorithm" color="text-[#6CC417]" />
+
+            <div className="left-2 mt-10 desktop:ml-32 desktop:w-4/6 bigmonitor:ml-48 bigmonitor:mt-16 bigmonitor:w-1/2">
+              <p>
+                I implemented an algorithm that takes a user&apos;s input as a
+                song title and returns the list of songs that should be played
+                in order; like a radio.
+              </p>
+              <br />
+              <p>
+                The algorithm uses a dataset of songs that I found online
+                (originally obtained from Spotify) to build a{" "}
+                <a
+                  href="https://www.dgp.toronto.edu/public_user/JamesStewart/378notes/16redBlack/"
+                  className="hover:text-[#6CC417]"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  Red-Black tree
+                </a>{" "}
+                based on how close each song in the dataset are to the initial
+                song specified by the user. Two songs being <em>close</em> is
+                defined as having similar characteristics in terms of attributes
+                such as acousticness or tempo. By playing songs in order from
+                closest to furthest, I expect the radio to play in such a way
+                that transitions between each song are smooth and the atmosphere
+                is never abruptly ruined.
+              </p>
+              <br />
+              <p>
+                I chose RB trees as the appropriate data structure to use in
+                this application because it guarantees the deepest branch to be
+                only at most twice as deep as the shallowest branch. This makes
+                the structure suitable to deal with large datasets, such as a
+                catalogue of songs.
+              </p>
+              <br />
+              <Image
+                src={Songrec}
+                alt=""
+                style={{
+                  maxWidth: "100%",
+                  height: "auto",
+                }}
+              />
+              <br />
+              <p>
+                <a
+                  href="https://drive.google.com/file/d/1CR7qfRLxsezkonrlolWPJBGjj7M9bC_e/view?usp=sharing"
+                  className="text-blue-300"
+                >
+                  code
+                </a>
+              </p>
+            </div>
+
+            <Footer />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
