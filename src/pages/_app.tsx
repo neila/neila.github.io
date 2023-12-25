99b77ef1e26f05b537077c4b8c17a8384d72af3a
neila <neila@shiftbase.xyz>
2023-12-25 16:19:04 +0900

    add debug pages

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,25 @@
 import { AppProps } from "next/app";
 
+import { useEffect, useState } from "react";
 import "../globals.css";
 
+export const config = {
+  unstable_runtimeJS: false,
+};
+
 const PF = ({ Component, pageProps }: AppProps) => {
+  const [mounted, setMounted] = useState(false);
+  const [mountCount, setMountCount] = useState(0);
+
+  // biome-ignore lint/correctness/useExhaustiveDependencies: <intended only to run on initial mount>
+  useEffect(() => {
+    setMounted(!mounted);
+    setMountCount(mountCount + 1);
+  }, []);
+
   return (
     <main>
+      <p>{mountCount}</p>
       <Component {...pageProps} />
     </main>
   );
