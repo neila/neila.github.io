2828bd3550de196e1b7691fc9f08ae0a0764b592
neila <40727091+neila@users.noreply.github.com>
2024-01-03 16:51:52 +0900

    add boilerplate blog

---
+++ src/pages/blog/[id].tsx
@@ -0,0 +1,63 @@
+import BaseLayout from "@/components/layouts/Base";
+import Graph from "@/utils/Graph";
+import { getAllPosts } from "@/utils/postid";
+import { NextPage } from "next";
+import { MDXRemote, MDXRemoteSerializeResult } from "next-mdx-remote";
+import { serialize } from "next-mdx-remote/serialize";
+
+type PostProps = {
+  content: MDXRemoteSerializeResult<
+    Record<string, unknown>,
+    Record<string, unknown>
+  >;
+  id: string;
+};
+
+const post: NextPage<PostProps> = ({ content, id }) => {
+  return (
+    <>
+      <BaseLayout>
+        <div className="mt-8 mx-auto desktop:w-2/3">
+          <article className="px-2">
+            <MDXRemote {...content} />
+          </article>
+
+          <div id="graph-container">
+            {/*display post network graph*/}
+            <Graph />
+          </div>
+        </div>
+      </BaseLayout>
+    </>
+  );
+};
+
+export async function getStaticProps(context) {
+  const { params } = context;
+  const { data, content, id } = getAllPosts().find(
+    (item) => item.id === params.id,
+  );
+  const mdxSource = await serialize(content);
+
+  return {
+    props: {
+      content: mdxSource,
+      id: id,
+    },
+  };
+}
+
+export async function getStaticPaths() {
+  const posts = getAllPosts();
+
+  return {
+    paths: posts.map((item) => ({
+      params: {
+        id: item.id,
+      },
+    })),
+    fallback: false,
+  };
+}
+
+export default post;
