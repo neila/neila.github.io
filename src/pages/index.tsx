96005759b5c8aa00ae12f531d55edb43cff7fe56
neila <40727091+neila@users.noreply.github.com>
2024-01-03 16:51:51 +0900

    migrate types and adjust landing

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -22,22 +22,24 @@ const HomePage: NextPage = () => {
   };
   return (
     <>
-      <h1 className="top-heading pt-12 px-4 -mb-8">Neila</h1>
-      <BaseLayout className="px-4">
-        {/* abstract */}
-        <section id="abstract">
-          <p>This is the website of neila.</p>
-          <p>
-            {/* For information about this website's philosophy & method, see the{" "}
+      <article>
+        <h1 className="top-heading pt-12 px-4 -mb-8">Neila</h1>
+        <BaseLayout className="px-4">
+          {/* abstract */}
+          <section id="abstract">
+            <p>This is the website of neila.</p>
+            <p>
+              {/* For information about this website's philosophy & method, see the{" "}
             <Link href="/about">About page</Link>.<br /> */}
-            {/* For website features & implementation, see the{" "}
+              {/* For website features & implementation, see the{" "}
             <Link href="/design">Design page</Link>.<br /> */}
-            For information about myself and contact information see{" "}
-            <Link href="/me">My page</Link>.
-            {/* <br />For site updates, see the <Link href="/changelog">Changelog</Link>. */}
-          </p>
-        </section>
-      </BaseLayout>
+              For information about myself and contact information see{" "}
+              <Link href="/me">My page</Link>.
+              {/* <br />For site updates, see the <Link href="/changelog">Changelog</Link>. */}
+            </p>
+          </section>
+        </BaseLayout>
+      </article>
     </>
   );
 };
