f2e37d7975582ad373185e8bba2a7e69fa43ac67
neila <40727091+neila@users.noreply.github.com>
2024-01-03 16:51:48 +0900

    add boilerplate page

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -1,5 +1,30 @@
+import BaseLayout from "@/components/layouts/Base";
+import Link from "next/link";
+
 const AboutSitePage = () => {
-  return <div>About this site</div>;
+  return (
+    <>
+      <BaseLayout>
+        <h1 className="top-heading text-center leading-[8rem]">
+          {/* TODO (5): automate capitalization of first word in headings */}
+          <span className="text-[3rem]">A</span>BOUT{" "}
+          <span className="text-[3rem]">T</span>HIS{" "}
+          <span className="text-[3rem]">W</span>EBSITE
+        </h1>
+
+        <p>
+          This page is about nei.la content;{" "}
+          {/* for the details of its
+          implementatioan & design, see the{" "}
+          <Link href="/about">Design page</Link>; and  */}
+          for information about me, see <Link href="/me">My page</Link>.
+        </p>
+
+        {/* <h1>The content</h1>
+        <h1>Technical specfictions</h1> */}
+      </BaseLayout>
+    </>
+  );
 };
 
 export default AboutSitePage;
--- src/pages/changelog/index.tsx
+++ src/pages/changelog/index.tsx
@@ -1,5 +1,15 @@
-const ChangeLogPage = () => {
-  return <div>Changelog</div>;
+import BaseLayout from "@/components/layouts/Base";
+
+const ChangelogPage = () => {
+  return (
+    <>
+      <BaseLayout>
+        <h1 className="top-heading text-center">
+          <span className="text-[3rem]">C</span>HANGELOG
+        </h1>
+      </BaseLayout>
+    </>
+  );
 };
 
-export default ChangeLogPage;
+export default ChangelogPage;
