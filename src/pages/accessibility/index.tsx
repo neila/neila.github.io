f9a67df5b4b66dcf5fb034cdf2ac294fd7bf1471
neila <40727091+neila@users.noreply.github.com>
2024-01-03 16:51:58 +0900

    add privacy and accessbility files

---
+++ src/pages/accessibility/index.tsx
@@ -0,0 +1,31 @@
+import BaseLayout from "@/components/layouts/Base";
+import { NextPage } from "next";
+
+import Link from "next/link";
+
+const AccessibilityPage: NextPage = () => {
+  return (
+    <>
+      <article>
+        <BaseLayout className="px-4">
+          {/* TODO (3): Probably turn this page into the site design page */}
+          <section id="accessibility">
+            <h1>Accessibility</h1>
+
+            <p>
+              Semantic markup is used wherever possible. <br />
+              JavaScript is not required for the core reading experience, and is
+              only used for optional features: comments, animations, and so on.
+              <br />
+              Progressive Enhacement: Pages can be read on various screen
+              widths; monitors, tablets, and smartphones alike. It even works on
+              a text browser like <a href="https://www.brow.sh/">browsh</a>.
+            </p>
+          </section>
+        </BaseLayout>
+      </article>
+    </>
+  );
+};
+
+export default AccessibilityPage;
