db8bcb91663143b14a29dc51d7ebf4293865e1cc
neila <40727091+neila@users.noreply.github.com>
2024-01-08 22:49:47 +0900

    add pagetitle prop

--- src/components/layouts/Base/index.tsx
+++ src/components/layouts/Base/index.tsx
@@ -3,14 +3,16 @@ import Header from "@/components/Header";
 import Head from "next/head";
 import { FC, ReactNode } from "react";
 
-const BaseLayout: FC<{ className?: string; children: ReactNode }> = ({
-  className,
-  children,
-}) => {
+const BaseLayout: FC<{
+  pageTitle?: string;
+  className?: string;
+  children: ReactNode;
+}> = ({ pageTitle, className, children }) => {
   return (
     <>
       <Head>
-        <title>Neila</title>
+        <title>{pageTitle ? `${pageTitle} | ` : ""}neila.github.io</title>
+        <meta name="description" content="website by neila" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
