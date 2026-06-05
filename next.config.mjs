fc70210752fadbfc8e523fa48b68ee7c66e656d8
neila <40727091+neila@users.noreply.github.com>
2026-06-05 16:55:15 +0900

    remote patterns remove

--- next.config.mjs
+++ next.config.mjs
@@ -13,16 +13,5 @@ export default withMDX({
   pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
   images: {
     unoptimized: true,
-    remotePatterns: [
-      new URL('https://avatars.githubusercontent.com/**'),
-      new URL('https://cdn-icons-png.flaticon.com/**'),
-      new URL('https://firebase.google.com/**'),
-      new URL('https://gist.githubusercontent.com/**'),
-      new URL('https://raw.githubusercontent.com/**'),
-      new URL('https://starbeamrainbowlabs.com/**'),
-      new URL('https://upload.wikimedia.org/**'),
-      new URL('https://www.r-project.org/**'),
-      new URL('https://www.rust-lang.org/**'),
-    ],
   },
 });
