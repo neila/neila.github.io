08513bcbfbd026a0736774b628ebea7868ce1f77
neila <40727091+neila@users.noreply.github.com>
2026-06-08 15:52:27 +0900

    trailing slash

--- next.config.mjs
+++ next.config.mjs
@@ -10,6 +10,7 @@ const withMDX = createMDX({
 export default withMDX({
   reactStrictMode: true,
   output: 'export',
+  trailingSlash: true,
   pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
   images: {
     unoptimized: true,
