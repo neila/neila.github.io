387e858af85f879ccc7af57eb2e7a2ca55424bd4
neila <40727091+neila@users.noreply.github.com>
2026-06-09 22:36:10 +0900

    path fix

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -3,12 +3,12 @@ const path = require('node:path');
 const buildLintCommand = (filenames) =>
   `yarn run biome lint ${filenames
     .map((f) => path.relative(process.cwd(), f))
-    .join(' ')}`;
+    .join(' ')} --no-errors-on-unmatched`;
 
 const buildFormatCommand = (filenames) =>
   `yarn run biome check ${filenames
     .map((f) => path.relative(process.cwd(), f))
-    .join(' ')} `;
+    .join(' ')} --no-errors-on-unmatched`;
 
 module.exports = {
   // biome-ignore lint/complexity/noCommaOperator: <>
